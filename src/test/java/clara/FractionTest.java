67a069278757e157f00950106c202a778ba9c807
Clara <clara.fang@gmail.com>
2021-01-09 17:25:11 -0500

    Separated tests by method

--- src/test/java/clara/FractionTest.java
+++ src/test/java/clara/FractionTest.java
@@ -7,16 +7,9 @@ import static org.junit.Assert.*;
 public class FractionTest {
 
     @Test
-    public void fraction() {
+    public void setterTest() {
         Fraction fraction = new Fraction(12, 12);
         Fraction fraction1 = new Fraction(1, 2);
-        Fraction fraction2 = new Fraction(2, 2);
-        Fraction fraction3 = new Fraction(3, 2);
-
-        assertTrue(fraction.toString().contains("12/12"));
-
-        assertEquals(12, fraction.getNumerator());
-        assertEquals(12, fraction.getDenominator());
 
         int newNumerator = 12;
         fraction.setNumerator(newNumerator);
@@ -27,14 +20,6 @@ public class FractionTest {
         assertEquals(fraction.getDenominator(), newDenominator);
 
         assertEquals(fraction.simplify(), fraction1);
-
-        assertEquals(fraction1.add(fraction2), fraction3);
-        assertEquals(fraction3.subtract(fraction1), fraction2);
-        assertEquals(fraction1.multiply(fraction2), new Fraction(2, 4));
-        assertEquals(fraction1.divide(fraction3), new Fraction(2, 6));
-
-        assertEquals(0.5, fraction1.convertToDecimal(), 1e-15);
-        assertEquals(1.0, fraction2.convertToDecimal(), 1e-15);
 
         try {
             fraction.setDenominator(0);
@@ -49,5 +34,51 @@ public class FractionTest {
         } catch (IllegalArgumentException e) {
             // expected
         }
+    }
+
+    @Test
+    public void getterTest() {
+        Fraction fraction = new Fraction(12, 12);
+        assertEquals(12, fraction.getNumerator());
+        assertEquals(12, fraction.getDenominator());
+    }
+
+    @Test
+    public void basicOperationsTest() {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 2);
+        Fraction fraction3 = new Fraction(3, 2);
+
+        assertEquals(fraction1.add(fraction2), fraction3);
+        assertEquals(fraction3.subtract(fraction1), fraction2);
+        assertEquals(fraction1.multiply(fraction2), new Fraction(2, 4));
+        assertEquals(fraction1.divide(fraction3), new Fraction(2, 6));
+    }
+
+    @Test
+    public void simplifyTest() {
+        Fraction fraction = new Fraction(6, 12);
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 2);
+        Fraction fraction3 = new Fraction(3, 2);
+
+        assertEquals(fraction.simplify(), fraction1);
+        assertEquals(fraction1.simplify(), fraction1);
+        assertEquals(fraction2.simplify(), new Fraction(1, 1));
+        assertEquals(fraction3.simplify(), fraction3);
+    }
+
+    @Test
+    public void convertToDecimalTest() {
+        Fraction f1 = new Fraction(1, 5);
+        assertEquals(0.2, f1.convertToDecimal(), 0.000000003);
+
+        f1.setNumerator(2);
+        f1.setDenominator(2);
+        assertEquals(1.0, f1.convertToDecimal(), 0.000000003);
+
+        f1.setNumerator(1);
+        f1.setDenominator(2);
+        assertEquals(0.5, f1.convertToDecimal(), 0.000000003);
     }
 }
