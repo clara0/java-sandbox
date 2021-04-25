0c11458c191dbc0ee5a3910092cfe246a4d6101b
clara0 <clara.fang@gmail.com>
2021-04-25 18:53:42 -0400

    added shuffleArray method

--- src/main/java/clara/ShuffleList.java
+++ src/main/java/clara/ShuffleList.java
@@ -1,13 +1,14 @@
 package clara;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ShuffleList {
 
     public List<Object> shuffleList(List<Object> sortedList) {
         Random random = new Random();
+        if (sortedList.isEmpty() || sortedList.size() == 1) {
+            return sortedList;
+        }
         List<Object> shuffledList = new ArrayList<>();
         while (sortedList.size() != 0) {
             int randomIndex = random.nextInt(sortedList.size());
@@ -16,5 +17,21 @@ public class ShuffleList {
             shuffledList.add(item);
         }
         return shuffledList;
+    }
+
+    public Object[] shuffleArray(Object[] sortedArray) {
+        Random random = new Random();
+        List<Object> sortedList = new ArrayList<>(Arrays.asList(sortedArray).subList(0, sortedArray.length));
+        if (sortedList.isEmpty() || sortedList.size() == 1) {
+            return sortedList.toArray();
+        }
+        List<Object> shuffledList = new ArrayList<>();
+        while (sortedList.size() != 0) {
+            int randomIndex = random.nextInt(sortedList.size());
+            Object item = sortedList.get(randomIndex);
+            sortedList.remove(randomIndex);
+            shuffledList.add(item);
+        }
+        return shuffledList.toArray();
     }
 }
--- src/test/java/clara/ShuffleListTest.java
+++ src/test/java/clara/ShuffleListTest.java
@@ -19,13 +19,31 @@ public class ShuffleListTest {
         objects.add("New York City");
         objects.add("Orlando");
         assertNotEquals(objects, shuffleList.shuffleList(objects));
-
         objects.clear();
+
         objects.add(1);
         objects.add(2);
         objects.add(3);
         objects.add(4);
         objects.add(5);
         assertNotEquals(objects, shuffleList.shuffleList(objects));
+        objects.clear();
+
+        assertEquals(objects, shuffleList.shuffleList(objects));
+
+        objects.add(1);
+        assertEquals(objects, shuffleList.shuffleList(objects));
+    }
+
+    @Test
+    public void shuffleArray() {
+        Object[] objectArray = new Object[]{1, 2, 3, 4, 5};
+        assertNotEquals(objectArray, shuffleList.shuffleArray(objectArray));
+
+        Object[] objectArray1 = new Object[]{1};
+        assertEquals(objectArray1, shuffleList.shuffleArray(objectArray1));
+
+        Object[] objectArray2 = new Object[]{};
+        assertEquals(objectArray2, shuffleList.shuffleArray(objectArray2));
     }
 }
