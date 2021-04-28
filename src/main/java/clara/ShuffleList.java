76743e79c1acf55125d02f6bc354fa6571c1d2ac
clara0 <clara.fang@gmail.com>
2021-04-27 20:08:56 -0400

    shortened shuffleList method

--- src/main/java/clara/ShuffleList.java
+++ src/main/java/clara/ShuffleList.java
@@ -5,24 +5,14 @@ import java.util.*;
 public class ShuffleList {
 
     public List<Object> shuffleList(List<Object> sortedList) {
-        Random random = new Random();
-        if (sortedList.isEmpty() || sortedList.size() == 1) {
-            return sortedList;
-        }
-        List<Object> shuffledList = new ArrayList<>();
-        while (sortedList.size() != 0) {
-            int randomIndex = random.nextInt(sortedList.size());
-            Object item = sortedList.get(randomIndex);
-            sortedList.remove(randomIndex);
-            shuffledList.add(item);
-        }
-        return shuffledList;
+        Object[] shuffledArray = shuffleArray(sortedList.toArray());
+        return Arrays.asList(shuffledArray);
     }
 
     public Object[] shuffleArray(Object[] sortedArray) {
         Random random = new Random();
         List<Object> sortedList = new ArrayList<>(Arrays.asList(sortedArray).subList(0, sortedArray.length));
-        if (sortedList.isEmpty() || sortedList.size() == 1) {
+        if (sortedList.size() <= 1) {
             return sortedList.toArray();
         }
         List<Object> shuffledList = new ArrayList<>();
