efbbb9cddee1dc7cdd24141b88e4866f7cf673e6
clara0 <clara.fang@gmail.com>
2021-08-11 16:36:46 -0400

    made a few changes for readability and efficiency

--- src/test/java/clara/SystemPropertiesTest.java
+++ src/test/java/clara/SystemPropertiesTest.java
@@ -5,17 +5,16 @@ import org.junit.Test;
 import java.util.*;
 
 public class SystemPropertiesTest {
-    Properties props = System.getProperties();
-    Set<Object> keys = props.keySet();
-    List<Object> keysList = new ArrayList<>(keys);
+    private final Properties properties = System.getProperties();
+    private final Set<String> keys = properties.stringPropertyNames();
+    private final List<String> keysList = new ArrayList<>(keys);
 
     /***
      * Sorts system properties by key in ascending alphabetical order.
      */
     @Test
     public void sortTest() {
-        TreeMap<Object, Object> sysProps = new TreeMap<>();
-        props.forEach(sysProps::put);
+        TreeMap<Object, Object> sysProps = new TreeMap<>(properties);
         sysProps.forEach((k, v) -> System.out.println(k + ": " + v));
     }
 
@@ -26,7 +25,7 @@ public class SystemPropertiesTest {
     public void sortDescendingTest() {
         keysList.sort(Collections.reverseOrder());
         for (Object k : keysList) {
-            System.out.println(k + ": " + props.get(k));
+            System.out.println(k + ": " + properties.get(k));
         }
     }
 
@@ -35,16 +34,13 @@ public class SystemPropertiesTest {
      */
     @Test
     public void sortKeyLengthTest() {
-        List<String> strKeyList = new ArrayList<>();
-        keysList.forEach(k -> strKeyList.add(k.toString()));
-        strKeyList.sort(new comp());
-
-        for (Object k : strKeyList) {
-            System.out.println(k + ": " + props.get(k));
+        keysList.sort(new CompareStrLengths());
+        for (Object k : keysList) {
+            System.out.println(k + ": " + properties.get(k));
         }
     }
 
-    class comp implements Comparator<String> {
+    static class CompareStrLengths implements Comparator<String> {
         public int compare(String o1, String o2) {
             return Integer.compare(o1.length(), o2.length());
         }
