9ced3986742d5fe46f8bb509c98daabeb8928690
clara0 <clara.fang@gmail.com>
2021-09-04 17:37:46 -0400

    Moved grade to a variable, added default case, and added loop

--- src/test/java/clara/SwitchTest.java
+++ src/test/java/clara/SwitchTest.java
@@ -2,6 +2,8 @@ package clara;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -11,7 +13,7 @@ public class SwitchTest {
 
     /**
      * Takes a random {@code int} from 0 to 5 (inclusive) and prints out a
-     * corresponding grade.
+     * corresponding grade using {@code switch}.
      */
     @Test
     public void findGrade() {
@@ -39,6 +41,31 @@ public class SwitchTest {
                 default:
                     grade = "Invalid";
             }
+            System.out.printf("Grade: %s%n%n", grade);
+        }
+    }
+
+    /**
+     * Takes a random {@code int} from 0 to 5 (inclusive) and prints out a
+     * corresponding grade using a map
+     */
+    @Test
+    public void findGradeMap() {
+        Random random = new Random();
+
+        for (int i = 1; i <= 20; i++) {
+            int num = random.nextInt(6);
+            System.out.printf("Number: %s%n", num);
+
+            Map<Integer, String> gradeMap = new HashMap<>();
+            gradeMap.put(1, "F");
+            gradeMap.put(2, "D");
+            gradeMap.put(3, "C");
+            gradeMap.put(4, "B");
+            gradeMap.put(5, "A");
+
+            String grade;
+            grade = gradeMap.getOrDefault(num, "Invalid");
             System.out.printf("Grade: %s%n%n", grade);
         }
     }
