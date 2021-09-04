0f9d3babf6109262df3d9cf3f73342caa99362ae
clara0 <clara.fang@gmail.com>
2021-09-04 17:18:12 -0400

    Moved grade to a variable, added default case, and added loop

--- src/test/java/clara/SwitchTest.java
+++ src/test/java/clara/SwitchTest.java
@@ -16,27 +16,30 @@ public class SwitchTest {
     @Test
     public void findGrade() {
         Random random = new Random();
-        int i = random.nextInt(6);
-        System.out.printf("Number: %s%n", i);
-        switch (i) {
-            case 0:
-                System.out.println("Grade: Invalid");
-                break;
-            case 1:
-                System.out.println("Grade: F");
-                break;
-            case 2:
-                System.out.println("Grade: D");
-                break;
-            case 3:
-                System.out.println("Grade: C");
-                break;
-            case 4:
-                System.out.println("Grade: B");
-                break;
-            case 5:
-                System.out.println("Grade: A");
-                break;
+        for (int i = 1; i <= 20; i++) {
+            int num = random.nextInt(6);
+            System.out.printf("Number: %s%n", num);
+            String grade;
+            switch (num) {
+                case 1:
+                    grade = "F";
+                    break;
+                case 2:
+                    grade = "D";
+                    break;
+                case 3:
+                    grade = "C";
+                    break;
+                case 4:
+                    grade = "B";
+                    break;
+                case 5:
+                    grade = "A";
+                    break;
+                default:
+                    grade = "Invalid";
+            }
+            System.out.printf("Grade: %s%n%n", grade);
         }
     }
 }
