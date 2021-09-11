9be275871da5b1cdd6b1f931f3b9e6babb5bf16f
clara0 <clara.fang@gmail.com>
2021-09-11 11:44:18 -0400

    Reverted java version back to 8

--- src/main/java/clara/util/NumberUtils.java
+++ src/main/java/clara/util/NumberUtils.java
@@ -220,7 +220,10 @@ public final class NumberUtils {
         }
         int days;
         switch (month) {
-            case 4, 6, 9, 11:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
                 days = 30;
                 break;
             case 2:
