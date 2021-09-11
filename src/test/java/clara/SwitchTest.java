d56e6fca9e8839cb17ec96f8dc4f4220176027b9
clara0 <clara.fang@gmail.com>
2021-09-11 10:18:15 -0400

    used @NotNull annotation to verify input was not null

--- src/test/java/clara/SwitchTest.java
+++ src/test/java/clara/SwitchTest.java
@@ -14,6 +14,16 @@ public class SwitchTest {
     /**
      * Takes a random {@code int} from 0 to 5 (inclusive) and prints out a
      * corresponding grade using {@code switch}.
+     * <p>
+     * Key:
+     * <ol>
+     *     <li>F</li>
+     *     <li>D</li>
+     *     <li>C</li>
+     *     <li>B</li>
+     *     <li>A</li>
+     * </ol>
+     * </p>
      */
     @Test
     public void findGrade() {
@@ -21,33 +31,23 @@ public class SwitchTest {
         for (int i = 1; i <= 20; i++) {
             int num = random.nextInt(6);
             System.out.printf("Number: %s%n", num);
-            char grade;
-            switch (num) {
-                case 1:
-                    grade = 'F';
-                    break;
-                case 2:
-                    grade = 'D';
-                    break;
-                case 3:
-                    grade = 'C';
-                    break;
-                case 4:
-                    grade = 'B';
-                    break;
-                case 5:
-                    grade = 'A';
-                    break;
-                default:
-                    grade = 'X';
-            }
+            char grade = switch (num) {
+                case 1 -> 'F';
+                case 2 -> 'D';
+                case 3 -> 'C';
+                case 4 -> 'B';
+                case 5 -> 'A';
+                default -> 'X';
+            };
             System.out.printf("Grade: %c%n%n", grade);
         }
     }
 
     /**
      * Takes a random {@code int} from 0 to 5 (inclusive) and prints out a
-     * corresponding grade using a map
+     * corresponding grade using map.
+     *
+     * @see SwitchTest#findGrade()
      */
     @Test
     public void findGradeMap() {
