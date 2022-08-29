a55f219e94d5074c154d858d6e6cf747ed0dcca2
Clara Fang <68715004+clara0@users.noreply.github.com>
2022-08-29 17:02:16 -0400

    check that shuffled lists are different from the original (#7)

--- src/main/java/clara/ShuffleList.java
+++ src/main/java/clara/ShuffleList.java
@@ -1,27 +1,37 @@
 package clara;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class ShuffleList {
 
     public List<Object> shuffleList(List<Object> sortedList) {
-        Object[] shuffledArray = shuffleArray(sortedList.toArray());
-        return Arrays.asList(shuffledArray);
+        Random random = new Random();
+        if (sortedList.size() <= 1) {
+            return sortedList;
+        }
+        Object[] shuffledArray = new Object[sortedList.size()];
+        Set<Object> filledIndexes = new HashSet<>();
+        for (Object item: sortedList) {
+            int randIndex = random.nextInt(sortedList.size());
+            while (filledIndexes.contains(randIndex)) {
+                randIndex = random.nextInt(sortedList.size());
+            }
+            shuffledArray[randIndex] = item;
+            filledIndexes.add(randIndex);
+        }
+        List<Object> shuffledList = Arrays.asList(shuffledArray).subList(0, shuffledArray.length);
+        if (shuffledList.equals(sortedList)) {
+            shuffleList(sortedList);
+        }
+        return shuffledList;
     }
 
     public Object[] shuffleArray(Object[] sortedArray) {
-        Random random = new Random();
-        List<Object> sortedList = new ArrayList<>(Arrays.asList(sortedArray).subList(0, sortedArray.length));
-        if (sortedList.size() <= 1) {
-            return sortedList.toArray();
-        }
-        List<Object> shuffledList = new ArrayList<>();
-        while (sortedList.size() != 0) {
-            int randomIndex = random.nextInt(sortedList.size());
-            Object item = sortedList.get(randomIndex);
-            sortedList.remove(randomIndex);
-            shuffledList.add(item);
-        }
+        List<Object> shuffledList = shuffleList(Arrays.asList(sortedArray).subList(0, sortedArray.length));
         return shuffledList.toArray();
     }
 }
